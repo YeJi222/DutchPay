a7d0bbc126840b23bb92494ee43abc09e4a2e056
YeJi222 <70511859+YeJi222@users.noreply.github.com>
2023-05-29 14:17:30 +0900

    feat: change login&signUp layout setting

--- frontend/src/components/InputBox.jsx
+++ frontend/src/components/InputBox.jsx
@@ -5,33 +5,37 @@ function inputBox({inputTitle, boxTitleText, placeholder, isToggled, inputValue,
         <div>
             <p className='inputTitle' style={{ color: isToggled === "login" ? "#FFF6F6" : "#E3EAFF" }}>{inputTitle}</p>
             {boxName === "phone" || boxName === "bank" || boxName === "account" ? (
-                    <div className='inputBox' style={{ backgroundColor: "#E3EAFF" }}>
-                        <input className='inputTotal' style={{ backgroundColor: isToggled === "login" ? "#FFF6F6" : "#E3EAFF" }}
-                            placeholder={placeholder}
-                            value={inputValue}
-                            onChange={(e) => setFunc(e.target.value)}
-                            type={boxName === "phone" || boxName === "account" ? "number" : "text"}
-                        >
-                        </input>
+                <div className='inputBox' style={{ backgroundColor: "#E3EAFF" }}>
+                    <input className='inputTotal' style={{ backgroundColor: isToggled === "login" ? "#FFF6F6" : "#E3EAFF" }}
+                        placeholder={placeholder}
+                        value={inputValue}
+                        onChange={(e) => setFunc(e.target.value)}
+                        type={boxName === "phone" || boxName === "account" ? "number" : "text"}
+                    >
+                    </input>
+                </div>
+            ) : (
+                <div className='inputBox' style={{ backgroundColor: isToggled === "login" ? "#FFF6F6" : "#ABBFFF" }}>
+                    <div className='boxTitle'>
+                        <p className='boxTitleText'>{boxTitleText}</p>
                     </div>
-                ) : (
-                    <div className='inputBox' style={{ backgroundColor: isToggled === "login" ? "#FFF6F6" : "#ABBFFF" }}>
-                        <div className='boxTitle'>
-                            <p className='boxTitleText'>{boxTitleText}</p>
-                        </div>
-                        <input className='input' style={{ backgroundColor: isToggled === "login" ? "#FFF6F6" : "#ABBFFF" }}
-                            placeholder={placeholder}
-                            value={inputValue}
-                            onChange={(e) => setFunc(e.target.value)}
-                            type={boxName === "pw" ? "password" : "text"}
-                        >
-                        </input>
-                    </div>
-                )}
+                    <input className='input' style={{ backgroundColor: isToggled === "login" ? "#FFF6F6" : "#ABBFFF" }}
+                        placeholder={placeholder}
+                        value={inputValue}
+                        onChange={(e) => setFunc(e.target.value)}
+                        type={boxName === "pw" ? "password" : "text"}
+                    >
+                    </input>
+                </div>
+            )}
             
-            <div className='errorMessage' style={{display: confirmInput === "yes" ? "none" : "block" }}>
-            Please fill {inputTitle} in the blanks.
-            </div>
+            {confirmInput === "yes" ? (
+                <div className='errorMessage'></div>
+            ) : (
+                <div className='errorMessage'>
+                    Please fill {inputTitle} in the blanks.
+                </div>
+            )}
         </div>
     );
 }
